bebed11b04d931bf95d88c36ab2d067f01a5d51c
euberman <euberman@gmail.com>
2020-10-11 16:21:25 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,20 @@ require './lib/artist.rb'
 class Song
     attr_accessor :title, :artist
     
+    @@all = []
+
     def initialize(title = 'Title') 
         @title = title
-        @artist = artist
+        @artist = {}
+        @@all << self
     end
 
     def artist
         @artist
+    end
+
+    def self.all
+        @@all
     end
 
 end
